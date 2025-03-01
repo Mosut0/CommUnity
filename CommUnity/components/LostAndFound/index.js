81b79b57ab39744fec507d8a5babf3ca4abe3fa1
jonathancolasante <jonathan.colasante@gmail.com>
2025-03-01 16:02:50 -0500

    feat: Implemented forms for lost and found

---
+++ CommUnity/components/LostAndFound/index.js
@@ -0,0 +1,4 @@
+export { default as LostAndFoundForm } from './LostAndFoundForm';
+export { default as LostItemForm } from './LostItemForm';
+export { default as FoundItemForm } from './FoundItemForm';
+export * from './styles';
