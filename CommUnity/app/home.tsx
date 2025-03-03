728d47b1d9cc4024c845bd0fd4659b58c04f3b87
Vivek Bhandari <v_bhandari@outlook.com>
2025-03-03 15:29:08 -0500

    Added Lint, Type Check and Expo Doc Workflows

--- CommUnity/app/home.tsx
+++ CommUnity/app/home.tsx
@@ -1,5 +1,5 @@
 import 'react-native-url-polyfill/auto';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { useEffect, useState } from 'react';
 import { supabase } from '../lib/supabase';
 import { Session } from '@supabase/supabase-js';
