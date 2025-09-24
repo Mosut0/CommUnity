af3c37ec8eeaf9e1f34aee7b24c82d9a74316b28
Mosut0 <mos.yas123@gmail.com>
2025-09-24 10:49:00 -0400

    Added test cases and mocks for markers

---
+++ __mocks__/react-native-maps.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export const Marker = (props) => <div {...props} />;
+export const Callout = (props) => <div {...props} />;
+export const MapMarker = (props) => <div {...props} />;
+const MapView = (props) => <div {...props} />;
+export default MapView;
--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-  preset: 'react-native',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@expo|expo|expo-file-system|expo-modules-core|react-native-url-polyfill)/)',
-  ],
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-  },
-  silent: true,
+	preset: 'react-native',
+	testEnvironment: 'jsdom',
+	transform: {
+		'^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+	},
+	transformIgnorePatterns: [
+		'node_modules/(?!(react-native|@react-native|@expo|expo|expo-file-system|expo-modules-core|react-native-url-polyfill)/)'
+	],
+	setupFilesAfterEnv: ['./jest.setup.js'],
+	moduleNameMapper: {
+		'^@/(.*)$': '<rootDir>/$1',
+		'^react-native-maps$': '<rootDir>/__mocks__/react-native-maps.js',
+		'^@expo/vector-icons$': '<rootDir>/__mocks__/@expo/vector-icons.js',
+		'^expo-font$': '<rootDir>/__mocks__/expo-font.js'
+	},
+	silent: true,
 };
